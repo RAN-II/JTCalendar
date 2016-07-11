bd6f1bcc9f80ea81d1ed120a5695d91dd797c652
Alexander Rozhkov <arozhkov@Mac-mini-Alexander.local>
2016-07-11 16:20:57 +0300

    [fix] dayContent size on layoutSubviews

--- JTCalendar/Views/JTCalendarPageView.m
+++ JTCalendar/Views/JTCalendarPageView.m
@@ -15,7 +15,6 @@
 	UIView<JTCalendarWeekDay> *_weekDayView;
 	NSMutableArray *_weeksViews;
 	NSUInteger _numberOfWeeksDisplayed;
-//	UIView *_dayContentView;
 	BOOL _isAnimating;
 	NSUInteger _activeWeekIndex;
 }
@@ -75,7 +74,6 @@
 		for (int i = 0; i < MAX_WEEKS_BY_MONTH; ++i) {
 			UIView<JTCalendarWeek> *weekView = [_manager.delegateManager buildWeekView];
 			[_weeksViews addObject:weekView];
-//            weekView.backgroundColor = [UIColor colorWithRed:arc4random_uniform(255)/255.0 green:arc4random_uniform(255)/255.0 blue:arc4random_uniform(255)/255.0 alpha:1.0];
 			[self addSubview:weekView];
 
 			weekView.manager = _manager;
@@ -121,87 +119,88 @@
 }
 
 - (void)updateContentViewWithDate:(NSDate *)contentDate {
-    if (!_manager.settings.weekModeEnabled) {
-        [_dayContentView removeFromSuperview];
-        _dayContentView = [_manager.delegateManager buildDayContentView];
-        if (_dayContentView)
-            [self addSubview:_dayContentView];
-        [self layoutSubviews];
-        
-        _activeWeekIndex = 0;
-        _isAnimating     = YES;
-        
-        for (UIView<JTCalendarWeek> *view in _weeksViews) {
-            _activeWeekIndex++;
-            if ([_manager.dateHelper date:view.startDate isTheSameWeekThan:contentDate]) {
-                _activeWeekIndex--;
-                [view removeFromSuperview];
-                [self addSubview:view];
-                break;
-            }
-        }
-        
-        [UIView transitionWithView:self duration:.5 options:0 animations:^{
-            NSUInteger counter = 0;
-            CGFloat dayContentTop = .0;
-            for (UIView<JTCalendarWeek> *view in _weeksViews) {
-                BOOL isActiveWeek = [_manager.dateHelper date:view.startDate isTheSameWeekThan:contentDate];
-                BOOL isNeedShift = [_manager.dateHelper date:view.startDate isEqualOrBefore:contentDate];
-                if (isActiveWeek) {
-                    counter = 0;
-                    dayContentTop = _weekDayView.frame.size.height + view.frame.size.height;
-                }
-                if (isNeedShift) {
-                    CGRect frameRect = CGRectMake(isActiveWeek ? .0 : -view.frame.size.width - (_activeWeekIndex - counter) * view.frame.size.width * .5,
-                                                  isActiveWeek ? _weekDayView.frame.size.height : view.frame.origin.y * .5,
-                                                  view.frame.size.width,
-                                                  view.frame.size.height
-                                                  );
-                    view.frame = frameRect;
-                }
-                else {
-                    view.frame = CGRectMake(view.frame.size.width + counter * view.frame.size.width * .5,
-                                            view.frame.size.height * 1.5,
-                                            view.frame.size.width,
-                                            view.frame.size.height
-                                            );
-                    [view layoutIfNeeded];
-                }
-                counter++;
-            }
-            if (_dayContentView)
-                _dayContentView.frame = CGRectMake(.0, dayContentTop, self.frame.size.width, self.frame.size.height - dayContentTop);
-        } completion:^(BOOL finished) {
-            [_weeksViews exchangeObjectAtIndex:0 withObjectAtIndex:_activeWeekIndex];
-            _isAnimating = NO;
-            _manager.settings.weekModeEnabled = YES;
-//            [_manager reload];
-            [_manager setDate:contentDate];
-        }];
-    }
-    else {
-        [_weeksViews exchangeObjectAtIndex:0 withObjectAtIndex:_activeWeekIndex];
-        _manager.settings.weekModeEnabled = NO;
-        UIView *contentView = _dayContentView;
-        _dayContentView = nil;
-        [_manager reload];
-        [UIView transitionWithView:self duration:.5 options:0 animations:^{
-            [self layoutSubviews];
-            CGFloat dayContentTop = .0;
-            for (UIView<JTCalendarWeek> *view in _weeksViews) {
-                CGFloat viewBottom = CGRectGetMaxY(view.frame);
-                if (dayContentTop < viewBottom) {
-                    dayContentTop = viewBottom;
-                }
-                view.alpha = 1.0;
-                [view layoutIfNeeded];
-            }
-            if (contentView)
-                contentView.frame = CGRectMake(.0, dayContentTop, self.frame.size.width, self.frame.size.height - dayContentTop);
-        }
-         
-                        completion:nil];
-    }
+	if (!_manager.settings.weekModeEnabled) {
+		[_dayContentView removeFromSuperview];
+		_dayContentView = [_manager.delegateManager buildDayContentView];
+		if (_dayContentView) {
+			[self addSubview:_dayContentView];
+			_dayContentView.frame = CGRectMake(_dayContentView.frame.origin.x, self.frame.size.height, _dayContentView.frame.size.width, _dayContentView.frame.size.height);
+		}
+		[self layoutSubviews];
+
+		_activeWeekIndex = 0;
+		_isAnimating     = YES;
+
+		for (UIView<JTCalendarWeek> *view in _weeksViews) {
+			_activeWeekIndex++;
+			if ([_manager.dateHelper date:view.startDate isTheSameWeekThan:contentDate]) {
+				_activeWeekIndex--;
+				[view removeFromSuperview];
+				[self addSubview:view];
+				break;
+			}
+		}
+
+		[UIView transitionWithView:self duration:.5 options:0 animations:^{
+		         NSUInteger counter = 0;
+		         CGFloat dayContentTop = .0;
+		         for (UIView<JTCalendarWeek> *view in _weeksViews) {
+		                 BOOL isActiveWeek = [_manager.dateHelper date:view.startDate isTheSameWeekThan:contentDate];
+		                 BOOL isNeedShift = [_manager.dateHelper date:view.startDate isEqualOrBefore:contentDate];
+		                 if (isActiveWeek) {
+		                         counter = 0;
+		                         dayContentTop = _weekDayView.frame.size.height + view.frame.size.height;
+				 }
+		                 if (isNeedShift) {
+		                         CGRect frameRect = CGRectMake(isActiveWeek ? .0 : -view.frame.size.width - (_activeWeekIndex - counter) * view.frame.size.width * .5,
+		                                                       isActiveWeek ? _weekDayView.frame.size.height : view.frame.origin.y * .5,
+		                                                       view.frame.size.width,
+		                                                       view.frame.size.height
+		                                                       );
+		                         view.frame = frameRect;
+				 }
+		                 else {
+		                         view.frame = CGRectMake(view.frame.size.width + counter * view.frame.size.width * .5,
+		                                                 view.frame.size.height * 1.5,
+		                                                 view.frame.size.width,
+		                                                 view.frame.size.height
+		                                                 );
+		                         [view layoutIfNeeded];
+				 }
+		                 counter++;
+			 }
+		         if (_dayContentView)
+				 _dayContentView.frame = CGRectMake(.0, dayContentTop, self.frame.size.width, self.frame.size.height - dayContentTop);
+		 } completion:^(BOOL finished) {
+		         [_weeksViews exchangeObjectAtIndex:0 withObjectAtIndex:_activeWeekIndex];
+		         _isAnimating = NO;
+		         _manager.settings.weekModeEnabled = YES;
+		         [_manager setDate:contentDate];
+		 }];
+	}
+	else {
+		[_weeksViews exchangeObjectAtIndex:0 withObjectAtIndex:_activeWeekIndex];
+		_manager.settings.weekModeEnabled = NO;
+		UIView *contentView = _dayContentView;
+		_dayContentView                   = nil;
+		[_manager reload];
+		[UIView transitionWithView:self duration:.5 options:0 animations:^{
+		         [self layoutSubviews];
+		         CGFloat dayContentTop = .0;
+		         for (UIView<JTCalendarWeek> *view in _weeksViews) {
+		                 CGFloat viewBottom = CGRectGetMaxY(view.frame);
+		                 if (dayContentTop < viewBottom) {
+		                         dayContentTop = viewBottom;
+				 }
+		                 view.alpha = 1.0;
+		                 [view layoutIfNeeded];
+			 }
+		         if (contentView)
+				 contentView.frame = CGRectMake(.0, dayContentTop, self.frame.size.width, self.frame.size.height - dayContentTop);
+		 }
+
+		                completion:nil];
+	}
 }
 
 - (void)layoutSubviews {
@@ -209,28 +208,31 @@
 		return;
 	}
 
-	CGFloat y         = 0;
-	CGFloat weekWidth = self.frame.size.width;
+	CGFloat y                  = 0;
+	CGFloat weekWidth          = self.frame.size.width;
+
+	NSInteger numberOfWeeksMax = MIN(_manager.settings.pageViewNumberOfWeeks, MAX_WEEKS_BY_MONTH);
+	if (numberOfWeeksMax == 0) {
+		numberOfWeeksMax = [_manager.dateHelper numberOfWeeks:_date];
+	}
 
 	if (_manager.settings.pageViewHaveWeekDaysView) {
 		CGFloat weekDayHeight = _weekDayView.frame.size.height; // Force use default height
 
 		if (weekDayHeight == 0) { // Or use the same height than weeksViews
-			weekDayHeight = self.frame.size.height / (_numberOfWeeksDisplayed + 1);
+			weekDayHeight = self.frame.size.height / (numberOfWeeksMax + 1);
 		}
 
 		_weekDayView.frame = CGRectMake(0, 0, weekWidth, weekDayHeight);
 		y                  = weekDayHeight;
 	}
 
-	CGFloat weekHeight = (self.frame.size.height - y) / _numberOfWeeksDisplayed;
+	CGFloat weekHeight = (self.frame.size.height - y) / numberOfWeeksMax;
 
 	for (UIView *weekView in _weeksViews) {
 		weekView.frame = CGRectMake(0, y, weekWidth, weekHeight);
 		y             += weekHeight;
 	}
-	if (_dayContentView)
-		_dayContentView.frame = CGRectMake(.0, y, weekWidth, self.frame.size.height - y);
 }
 
 @end
